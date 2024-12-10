fb110340bccd4f2135900a360d87c2b30645a2fa
ngotrongtin <tigopro.1703@gmail.com>
2024-12-10 09:11:23 +0700

    Login

--- app/src/main/java/ttcn/hodangtai/mbookingadmim/LoginActivity.java
+++ app/src/main/java/ttcn/hodangtai/mbookingadmim/LoginActivity.java
@@ -1,10 +1,12 @@
 package ttcn.hodangtai.mbookingadmim;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +18,17 @@ import ttcn.hodangtai.mbookingadmim.viewmodels.MainViewModel;
 
 public class LoginActivity extends AppCompatActivity{
     private LoginViewModel viewModel;
+
+    private void switchToMainView(String username){
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+
+        // Truyền thêm dữ liệu nếu cần (ví dụ: username)
+        intent.putExtra("USERNAME", username);
+
+        startActivity(intent);
+
+        finish();
+    }
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,9 +60,21 @@ public class LoginActivity extends AppCompatActivity{
 
         // Handle button actions
         signInButton.setOnClickListener(v -> {
-            // Hiển thị EditText khi bấm vào nút Sign In
-            usernameEditText.setVisibility(View.VISIBLE);
-            passwordEditText.setVisibility(View.VISIBLE);
+            if (usernameEditText.getVisibility() != View.VISIBLE || passwordEditText.getVisibility() != View.VISIBLE) {
+                // Nếu chưa hiển thị, đặt EditText thành VISIBLE
+                usernameEditText.setVisibility(View.VISIBLE);
+                passwordEditText.setVisibility(View.VISIBLE);
+            } else {
+                // Nếu đã hiển thị, lấy dữ liệu từ EditText
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                if (username.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Vui lòng nhập tài khoản và mật khẩu!", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Gọi phương thức chuyển sang MainActivity
+                    switchToMainView(username);
+                }
+            }
         });
 
     }
